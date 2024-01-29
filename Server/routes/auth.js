7a758896282ad975b6d395ee00e1e2f3b6f65435
ahtesham345 <shank5445@gmail.com>
2024-01-29 15:59:01 +0500

    2nd commit

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -6,42 +6,48 @@ const authmiddleware = require("../middleware/auth.middleware");
 const router = Router();
 
 router.post("/login", async (req, res) => {
-    try {
-      const { email_address, password } = req.body;
-  
-      // Find the user by email
-      const user = await User.findOne({ email_address });
-  
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-  
-      // Check if the password is correct
-      const isPasswordCorrect = await bcrypt.compare(password, user.password);
-  
-      if (!isPasswordCorrect) {
-        return res.status(401).json({ message: "Invalid credentials" });
-      }
-  
-      // Create and sign a JWT token
-      const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
-  
-      res.status(200).json({ token });
-    } catch (error) {
-      res.status(500).json({ message: "Internal Server Error a raha hai " });
-    }
-  });
+  try {
+    const { email_address, password } = req.body;
 
-  router.post("/logout", authmiddleware, (req, res) => {
-    try {
-        // Optional: Add the current token to the blacklist
-        const token = req.headers.authorization.split(" ")[1];
-        blacklistedTokens.add(token);
+    // Find the user by email
+    const user = await User.findOne({ email_address });
 
-        res.status(200).json({ message: "Logout successful" });
-    } catch (error) {
-        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    // Check if the password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Create and sign a JWT token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error a raha hai " });
+  }
 });
-  
-  module.exports = router;
+
+router.post("/logout", authmiddleware, (req, res) => {
+  try {
+    // Optional: Add the current token to the blacklist
+    const token = req.headers.authorization.split(" ")[1];
+    blacklistedTokens.add(token);
+
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
+module.exports = router;
