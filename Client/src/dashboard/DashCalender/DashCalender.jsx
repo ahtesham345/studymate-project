e77f61a79eb8fb709295fc7cf21937ce77a96b4c
kakugb <karamat.4101@gmail.com>
2024-01-30 16:33:15 +0500

    2nd commit

--- Client/src/dashboard/DashCalender/DashCalender.jsx
+++ Client/src/dashboard/DashCalender/DashCalender.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { useState } from 'react'
-import '../../App.css'
-import Header from '../../dashboard/Header'
-import Sidebar from '../../dashboard/Sidebar'
-import Home from '../../dashboard/Home'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { useState } from "react";
+import "../../App.css";
+import Header from "../../dashboard/Header";
+import Sidebar from "../../dashboard/Sidebar";
 export default function DashboardComp() {
-    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
-    const OpenSidebar = () => {
-      setOpenSidebarToggle(!openSidebarToggle)
-    }
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
   return (
-    <div className='grid-container'>
-    <Header OpenSidebar={OpenSidebar}/>
-    <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-   <h1 className='w-full h-screen text-3xl font-extrabold text-white mx-auto '>Dashboard</h1>
-  </div>
-  )
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <h1 className="w-full h-screen text-3xl font-extrabold text-white mx-auto ">
+        Dashboard
+      </h1>
+    </div>
+  );
 }
-
-
--- Client/src/dashboard/Header.jsx
+++ Client/src/dashboard/Header.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import 
- { BsPersonCircle, BsSearch, BsJustify}
- from 'react-icons/bs'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Select, Option } from "@material-tailwind/react";
+import { BsPersonCircle, BsSearch, BsJustify } from "react-icons/bs";
 
-function Header({OpenSidebar}) {
+function Header({ OpenSidebar }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear the token from session storage
+    sessionStorage.removeItem("token");
+    // Redirect to the login page
+    navigate("/");
+  };
   return (
-    <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={OpenSidebar} />
+      </div>
+
+      <div className="w-full flex justify-end mt-2">
+        <BsPersonCircle className="icon w-10 h-10 absolute" />
+
+        <div className="border-none relative -right-1">
+          <Select className="border-none w-20 h-10 mt-1">
+            <Option>
+              <button onClick={handleLogout} className="w-14 h-8 font-bold">
+                Log Out
+              </button>
+            </Option>
+          </Select>
         </div>
-        <div className='header-left'>
-            <BsSearch  className='icon'/>
-        </div>
-        <div className='header-right'>
-            <BsPersonCircle className='icon w-10 h-10'/>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
